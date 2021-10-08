bd712523f0c1a3f803945032f19b2dfbfe646ccc
Cristóvão <cristovaofabio3@gmail.com>
2021-10-08 10:31:14 -0300

    melhorias na tela de login

--- lib/telas/Login.dart
+++ lib/telas/Login.dart
@@ -1,6 +1,7 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:olx/main.dart';
 import 'package:olx/model/Usuario.dart';
 import 'package:olx/util/BotaoCustomizado.dart';
 import 'package:olx/util/GeradorRotas.dart';
@@ -136,113 +137,144 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
+    var alturaDispositivo = MediaQuery.of(context).size.height;
+    var alturaBarraStatus = MediaQuery.of(context).padding.top;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(""),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: Center(
-          child: SingleChildScrollView(
+      backgroundColor: temaPadrao.primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            height: 130,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(bottom: 15),
-                  child: Image.asset(
-                    "imagens/logo.png",
-                    width: 200,
-                    height: 150,
-                  ),
-                ),
+              children: [
                 Padding(
                   padding: EdgeInsets.only(bottom: 10),
-                  child: InputCustomizado(
-                    "Digite o seu e-mail",
-                    Icon(Icons.email_outlined, color: Colors.black),
-                    controller: _controllerEmail,
-                    autofocus: true,
-                    type: TextInputType.emailAddress,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 10),
-                  child: InputCustomizado(
-                    "Digite uma senha",
-                    Icon(Icons.vpn_key, color: Colors.black),
-                    controller: _controllerSenha,
-                    maxLinhas: 1,
-                    obscure: true,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text("Logar"),
-                    Switch(
-                        value: _cadastrar,
-                        onChanged: (bool valor) {
-                          setState(() {
-                            _cadastrar = valor;
-                          });
-                        }),
-                    Text("Cadastrar"),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10, bottom: 20),
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: GestureDetector(
-                      onTap: () {
-                        //Abrir a página de recuperação de senha
-                        Navigator.pushNamed(
-                            context, GeradorRotas.ROTA_RECUPERAR_SENHA);
-                      },
-                      child: Text(
-                        "Esqueci minha senha!",
-                        style: TextStyle(
-                          color: Colors.deepPurple,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                  child: Text(
+                    "Seja bem-vindo!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
                     ),
                   ),
                 ),
-                AnimatedSwitcher(
-                  duration: const Duration(milliseconds: 200),
-                  transitionBuilder:
-                      (Widget child, Animation<double> animation) {
-                    return ScaleTransition(child: child, scale: animation);
-                  },
-                  child: _status
-                      ? TextButton(
-                          onPressed: () {},
-                          child: CircularProgressIndicator(
-                            color: Colors.deepPurple,
-                          ),
-                        )
-                      : BotaoCustomizado(
-                          texto: _cadastrar == false ? "Entrar" : "Cadastrar",
-                          onPressed: _validarCampos,
-                        ),
-                ),
                 Padding(
-                  padding: EdgeInsets.only(top: 16),
-                  child: Center(
+                    padding: EdgeInsets.only(bottom: 10),
                     child: Text(
-                      _mensagemErro,
-                      style: TextStyle(
-                        color: Colors.red[300],
-                        fontSize: 18,
-                      ),
+                      "Entre com o seu e-mail e senha, ou realize o seu cadastro.",
+                      style: TextStyle(fontSize: 15, color: Colors.white60),
                     ),
                   ),
-                ),
               ],
             ),
           ),
-        ),
+          Container(
+            height: (alturaDispositivo - alturaBarraStatus) -130,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(75),
+              ),
+            ),
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: ListView(
+                //primary: false,
+                children: <Widget>[
+                  SizedBox(height: 50,),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: InputCustomizado(
+                      "Digite o seu e-mail",
+                      Icon(Icons.email_outlined, color: Colors.black),
+                      controller: _controllerEmail,
+                      autofocus: true,
+                      type: TextInputType.emailAddress,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: InputCustomizado(
+                      "Digite uma senha",
+                      Icon(Icons.vpn_key, color: Colors.black),
+                      controller: _controllerSenha,
+                      maxLinhas: 1,
+                      obscure: true,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Logar"),
+                      Switch(
+                          value: _cadastrar,
+                          onChanged: (bool valor) {
+                            setState(() {
+                              _cadastrar = valor;
+                            });
+                          }),
+                      Text("Cadastrar"),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 20),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: GestureDetector(
+                        onTap: () {
+                          //Abrir a página de recuperação de senha
+                          Navigator.pushNamed(
+                              context, GeradorRotas.ROTA_RECUPERAR_SENHA);
+                        },
+                        child: Text(
+                          "Esqueci minha senha!",
+                          style: TextStyle(
+                            color: Colors.deepPurple,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  AnimatedSwitcher(
+                    duration: Duration(milliseconds: 200),
+                    transitionBuilder:
+                        (Widget child, Animation<double> animation) {
+                      return ScaleTransition(child: child, scale: animation);
+                    },
+                    child: _status
+                        ? TextButton(
+                            onPressed: () {},
+                            child: CircularProgressIndicator(
+                              color: Colors.deepPurple,
+                            ),
+                          )
+                        : BotaoCustomizado(
+                            texto: _cadastrar == false ? "Entrar" : "Cadastrar",
+                            onPressed: _validarCampos,
+                          ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Center(
+                      child: Text(
+                        _mensagemErro,
+                        style: TextStyle(
+                          color: Colors.red[300],
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
